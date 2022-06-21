e286b5f09eb254bf2e2da53018095c69038601ee
Marek Kabala <marekkabala1@gmail.com>
2022-06-21 06:12:55 +0100

    working on Gym Page

--- src/pages/GymPage.tsx
+++ src/pages/GymPage.tsx
@@ -1,22 +1,26 @@
 import { useEffect } from "react";
 import BottomMenu from "../components/BottomMenu";
 import { useAuth } from "../firebseConfig/AuthContext"
-import { GrAddCircle } from "react-icons/gr"
+import { IoIosAddCircleOutline } from "react-icons/io"
 
 const GymPage = () => {
     const currentUser = useAuth()
     useEffect(() => {
         if (currentUser) {
-            console.log(currentUser);
+
         }
     }, [currentUser])
+    const addWorkout = () => {
+        const newDiv = document.createElement('div')
+        newDiv.classList.add('gymPage_workout')
+    }
 
 
     return (
-        <section className="gym-section flex-column">
-            <div className="gym-sectionWrapper flex-column flex center">
-                <h1>GYM</h1>
-                <div><GrAddCircle />Add Workout</div>
+        <section className="gymPage-section flex-column">
+            <h1 className="gymPage_heading">GYM</h1>
+            <div className="gymPage-sectionWrapper flex-column center">
+                <div className="gymPage_add flex center " onClick={addWorkout}><IoIosAddCircleOutline /><span>Add Workout</span></div>
             </div>
             <BottomMenu />
         </section>
