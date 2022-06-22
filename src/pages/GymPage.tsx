b0e4893c4cafcbf41df05588e4b2171a5a4c80c9
Marek Kabala <marekkabala1@gmail.com>
2022-06-22 06:54:06 +0100

    addin element dynamicly not finished yet

--- src/pages/GymPage.tsx
+++ src/pages/GymPage.tsx
@@ -1,29 +1,46 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import BottomMenu from "../components/BottomMenu";
 import { useAuth } from "../firebseConfig/AuthContext"
 import { IoIosAddCircleOutline } from "react-icons/io"
 
 const GymPage = () => {
     const currentUser = useAuth()
+    const [workOutDiv, setWorkOutDiv] = useState<any>([])
     useEffect(() => {
         if (currentUser) {
 
         }
     }, [currentUser])
+
+
+    const newDiv = () => {
+        return (
+            <>
+                <input className="gymPage_workout" placeholder="Add workout name" />
+                <button>submit</button>
+            </>
+        )
+    }
+
     const addWorkout = () => {
-        const newDiv = document.createElement('div')
-        newDiv.classList.add('gymPage_workout')
+        return setWorkOutDiv(newDiv)
     }
 
 
     return (
-        <section className="gymPage-section flex-column">
-            <h1 className="gymPage_heading">GYM</h1>
-            <div className="gymPage-sectionWrapper flex-column center">
-                <div className="gymPage_add flex center " onClick={addWorkout}><IoIosAddCircleOutline /><span>Add Workout</span></div>
-            </div>
+        <>
+            <section className="gymPage-section flex-column center">
+                <h1 className="gymPage_heading">GYM</h1>
+                <div className="gymPage-sectionWrapper flex-column center">
+                    <div className="gymPage_add flex center " onClick={addWorkout}><IoIosAddCircleOutline /><span>Add Workout</span></div>
+                </div>
+                <div className="flex">
+                    {workOutDiv}
+                </div>
+
+            </section>
             <BottomMenu />
-        </section>
+        </>
     )
 }
 
