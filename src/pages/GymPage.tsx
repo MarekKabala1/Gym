0b45733cc9cb01aa72c580325562bf36e2c2bdd7
Marek Kabala <marekkabala1@gmail.com>
2022-06-23 07:01:16 +0100

    commit to check input in gym page

--- src/pages/GymPage.tsx
+++ src/pages/GymPage.tsx
@@ -6,6 +6,7 @@ import { IoIosAddCircleOutline } from "react-icons/io"
 const GymPage = () => {
     const currentUser = useAuth()
     const [workOutDiv, setWorkOutDiv] = useState<any>([])
+    const [typeOfWorkout, setTypeOfWorkout] = useState<string>()
     useEffect(() => {
         if (currentUser) {
 
@@ -15,11 +16,26 @@ const GymPage = () => {
 
     const newDiv = () => {
         return (
-            <>
-                <input className="gymPage_workout" placeholder="Add workout name" />
-                <button>submit</button>
-            </>
+            <form className="form_wrapper flex-column center" style={{ paddingTop: '1rem' }} onSubmit={submitTypeOfWorkout}>
+                <input
+                    className="gymPage_workout"
+                    placeholder="Workout type"
+                    type="text"
+                    required
+                    value={typeOfWorkout}
+                    onChange={(e) => setTypeOfWorkout(e.target.value)} />
+                <button
+                    className="btn"
+                    style={{ backgroundColor: 'lightgreen' }}
+                    type="submit">Add</button>
+            </form>
         )
+    }
+
+    const submitTypeOfWorkout = (e: { preventDefault: () => void; }) => {
+        e.preventDefault();
+        console.log('1');
+        return typeOfWorkout
     }
 
     const addWorkout = () => {
@@ -34,8 +50,8 @@ const GymPage = () => {
                 <div className="gymPage-sectionWrapper flex-column center">
                     <div className="gymPage_add flex center " onClick={addWorkout}><IoIosAddCircleOutline /><span>Add Workout</span></div>
                 </div>
-                <div className="flex">
-                    {workOutDiv}
+                <div style={{ gap: '1rem' }} className="flex-column center">
+                    {workOutDiv}{typeOfWorkout}
                 </div>
 
             </section>
