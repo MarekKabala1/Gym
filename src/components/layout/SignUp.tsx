39bbdd709f448c200a9f8a36e1d6c602136944c8
Marek Kabala <marekkabala1@gmail.com>
2022-08-26 05:35:42 +0100

    checking bug

--- src/components/layout/SignUp.tsx
+++ src/components/layout/SignUp.tsx
@@ -57,7 +57,7 @@ const SignUp = () => {
                 weekRutines: []
             })
             navigate(`/userpage/${user.uid}`)
-            return user
+            return newUser
         } catch {
             setError(error)
         }
@@ -69,7 +69,7 @@ const SignUp = () => {
         setRepetPassword('')
         setTimeout(() => {
             setError('')
-        }, 6000)
+        }, 3000)
     }
 
     return (
